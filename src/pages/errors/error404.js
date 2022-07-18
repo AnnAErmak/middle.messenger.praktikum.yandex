00399283c446d13369815414056cbd5e969ad76b
Anna Ermak <ermak-anna86@yandex.ru>
2022-07-18 12:58:17 +0300

    added sass styles header and errors

--- src/pages/errors/error404.js
+++ src/pages/errors/error404.js
@@ -1,5 +1,6 @@
 import template404 from './error404.hbs'
 import '../../globalStyles/globalStyles.scss'
+import './errors.scss'
 
 const html = template404();
 const root = document.querySelector('#root')
--- src/pages/errors/error500.js
+++ src/pages/errors/error500.js
@@ -1,5 +1,6 @@
 import template500 from "./error500.hbs";
 import '../../globalStyles/globalStyles.scss'
+import './errors.scss'
 
 const html = template500();
 const root = document.querySelector('#root')
