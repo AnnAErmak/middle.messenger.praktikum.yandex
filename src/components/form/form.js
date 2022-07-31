719e61a01b5f1e029189749628cb4a880731b2b1
Anna Ermak <ermak-anna86@yandex.ru>
2022-07-31 11:07:53 +0300

    add Eslint Stylelint

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,16 +1,17 @@
-import Block from "../../utils/Block";
-import formTemplate from './form.hbs'
-import {isValid} from "../../utils/validator";
+import Block from '../../utils/Block';
+import formTemplate from './form.hbs';
+import { isValid } from '../../utils/validator';
 
-export default class Form extends Block{
-    render(){
-        return this.compile(formTemplate, this.props);
-    }
-    _addEvents() {
-        super._addEvents()
-        this._element.querySelectorAll('input').forEach(input =>{
-            input.addEventListener('focus', e => isValid(e.target))
-            input.addEventListener('blur', e => isValid(e.target))
-        })
-    }
+export default class Form extends Block {
+  render() {
+    return this.compile(formTemplate, this.props);
+  }
+
+  _addEvents() {
+    super._addEvents();
+    this._element.querySelectorAll('input').forEach((input) => {
+      input.addEventListener('focus', (e) => isValid(e.target));
+      input.addEventListener('blur', (e) => isValid(e.target));
+    });
+  }
 }
